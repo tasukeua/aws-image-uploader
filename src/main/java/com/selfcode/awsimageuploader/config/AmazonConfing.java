29eb8590e6f799e42ba02de6c445f4d7f5d4bef1
Oleksii Rozumenko <oleksii.rozumenko@gmail.com>
2024-10-14 17:03:13 +0300

    Hide creds

--- src/main/java/com/selfcode/awsimageuploader/config/AmazonConfing.java
+++ src/main/java/com/selfcode/awsimageuploader/config/AmazonConfing.java
@@ -1,8 +1,7 @@
 package com.selfcode.awsimageuploader.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +11,17 @@ import org.springframework.context.annotation.Configuration;
 public class AmazonConfing {
 
     @Bean
+    public AWSCredentialsProvider amazonAWSCredentialsProvider() {
+
+        return DefaultAWSCredentialsProviderChain.getInstance();
+    }
+
+    @Bean
     public AmazonS3 s3() {
-        AWSCredentials credentials = new BasicAWSCredentials(
-                "AKIAYS2NW56WOIBNGAU6",
-                "s7fXK4XxTX2VBn2unzaT6cG8Iw3p3He2HnguYTkj"
-        );
         return AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withCredentials(amazonAWSCredentialsProvider())
+                .withRegion(Regions.US_EAST_1)
                 .build();
     }
 }
